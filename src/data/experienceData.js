72914f303c6416fe9c0d00199f396968a7d99c16
sowmyava-hub <123717995+sowmyava-hub@users.noreply.github.com>
2023-07-10 18:39:45 -0400

    First commit

---
+++ src/data/experienceData.js
@@ -0,0 +1,33 @@
+export const experienceData = [
+    {
+        id: 1,
+        company: 'Globex Corporation',
+        jobtitle: 'Frontend Developer',
+        startYear: '2018',
+        endYear: '2019',
+        details: "● Developed robust data ingestion pipelines using Airflow DAGs to process and load television data from AWS S3 into the system on a weekly basis, ensuring efficient processing for up-to-date and accurate information."+
+        "● Implemented automation of the email workflow for ad buyers by developing and deploying Python scripts, integrating"+
+        "with relevant APIs, and leveraging email scheduling techniques, resulting in a 50% reduction in manual effort."+
+        "● Utilized advanced SQL queries and data transformation techniques to efficiently extract and process over 10,000"+
+        "rows of television data daily from the redshift data warehouse, ensuring optimal performance and accuracy."+
+        "● Orchestrated seamless data migrations from the data warehouse to Metabase, successfully transferring and"+
+        "transforming 100 GB of data, enabling the data analytics team to generate comprehensive charts and dashboards"+
+        "for in-depth analysis and valuable business insights."
+    },
+    {
+        id: 2,
+        company: 'Vehement Capital Partners',
+        jobtitle: 'Backend Developer',
+        startYear: '2019',
+        endYear: '2020',
+        details: 'dfgh'
+    },
+    {
+        id: 3,
+        company: 'Wonka Industries',
+        jobtitle: 'Marketing',
+        startYear: '2020',
+        endYear: 'Present',
+        details: 'dfgh'
+    },
+]
---
+++ src/data/headerData.js
@@ -0,0 +1,10 @@
+import resume from '../assets/pdf/resume.pdf'
+import profile_photo from '../assets/png/profile.jpeg'
+
+export const headerData = {
+    name: 'Sowmya Iyer',
+    title: "Web Developer",
+    desciption:"There wasn't a bird in the sky, but that was not what caught her attention. It was the clouds. The deep green that isn't the color of clouds, but came with these. She knew what was coming and she hoped she was prepared. ",
+    image: profile_photo,
+    resumePdf: resume
+}
