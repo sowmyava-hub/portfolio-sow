dca425ae49725126d5c5ad9db9bfc32433006817
sowmyava-hub <123717995+sowmyava-hub@users.noreply.github.com>
2023-07-13 21:51:05 -0400

    testimonial

--- src/data/testimonialsData.js
+++ src/data/testimonialsData.js
@@ -36,20 +36,6 @@ export const testimonialsData = [
         title: 'Actor',
         text: "Making my portfolio with this helped me get my role as 'Dr. Drake Ramoray' in 'Days Of Our Lives'. You guys are the best !",
         image: boy1
-    },
-    {
-        id: 5,
-        name: 'Rachel Green',
-        title: 'Sales Executive',
-        text: 'Ralph Lauren was one of the best things that ever happened to me. My portfolio made that dream happen. I owe you guys.',
-        image: girl3
-    },
-    {
-        id: 6,
-        name: 'Ross Geller',
-        title: 'Paleontologist',
-        text: "Be it 'Rocks' or 'Bones', you need a website to display it. This is it. Great work guys !",
-        image: boy3
     }
 ]
 
